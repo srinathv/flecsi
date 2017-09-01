7849b81b6ce9d94479e58a4143635dcaa21c5931
Jonathan Pietarila Graham <jpietarilagraham@gmail.com>
2017-09-01 11:21:33 -0600

    Define future interface for reductions

--- flecsi/execution/test/reduction.cc
+++ flecsi/execution/test/reduction.cc
@@ -24,6 +24,17 @@ double local_value_task(
 }
 flecsi_register_task(local_value_task, loc, single|leaf);
 
+void check_values_task(
+    const double global_max,
+    const double global_min,
+    const int num_colors,
+    const int cycle)
+{
+  ASSERT_EQ(global_max, static_cast<double>(num_colors * cycle));
+  ASSERT_EQ(global_min, static_cast<double>(cycle));
+
+}
+flecsi_register_task(check_values_task, loc, single|leaf);
 
 //----------------------------------------------------------------------------//
 // User driver.
@@ -43,17 +54,13 @@ void driver(int argc, char ** argv) {
     auto global_max_future =
       flecsi::execution::context_t::instance().reduce_max(local_future);
     flecsi_future__<double> *flecsi_max_future = &global_max_future;
-    const bool silence_warnings = true;
-    const size_t index = 0;
-    double global_max = flecsi_max_future->get(index, silence_warnings);
 
     auto global_min_future =
       flecsi::execution::context_t::instance().reduce_min(local_future);
     flecsi_future__<double> *flecsi_min_future = &global_min_future;
-    double global_min = flecsi_min_future->get(index, silence_warnings);
- 
-    ASSERT_EQ(global_max, static_cast<double>(num_colors * cycle));
-    ASSERT_EQ(global_min, static_cast<double>(cycle));
+
+    flecsi_execute_task(check_values_task, single, flecsi_max_future, flecsi_min_future,
+        num_colors, cycle);
   } // cycle
 
 } // driver
